ab0746bc05cbb95cf9e2309031813ab4a078b44b
jkmiller-wildlife <jkmiller.wildlife@gmail.com>
2019-03-12 12:41:49 -0700

    FINAL! IT IS DONE!

--- Final_app/app.R
+++ Final_app/app.R
@@ -2,8 +2,6 @@ library(tidyverse)
 library(shiny)
 library(shinythemes)
 
-
-
 ############################################
 # Data 
 
@@ -13,7 +11,7 @@ birds_by_sector <- read_csv("birds_by_sector.csv")
 
 #birds_by_region$survey_week <- mdy(birds_by_region$survey_week)
 
-wrack_avian_tab3 <- read_csv("wrack_avian_df.csv")
+wrack_avian_tab3 <- read_csv("wrack_avian_fix.csv")
 
 #wrack_avian_tab3$survey_week <- mdy(wrack_avian_tab3$survey_week)
 
@@ -47,8 +45,11 @@ ui <- fluidPage(theme = shinytheme("superhero"), #Valid themes: cerulean, cosmo,
                                       h1("Data"), #subtitle
                                       p("Since March 2011, weekly transect surveys were conducted at VAFB beaches. North and South VAFB beaches were separated by distinct beach sectors. Each beach sector was divided into transect blocks approximately 100-300 meters in length along the coastal strand. Within each transect block, counts were taken of the number of snowy plovers, age, sex, flock size, presence of paired individuals, and presence of broods. Additionally, the number and species of shorebirds, seabird, or raptors utilizing the habitat was recorded, and the amount of wrack present on each block was scored (Robinette et al. 2017)"),
                                       p("Thousands of data points from bird count transects have been collected by field biologists since 2011. Bird counts have been aggregated and reported at the end of each transect and/or field survey. Data from weekly and semi-weekly surveys include: survey date and location, bird type (shorebird or gull), bird species, species/category abundance (recorded as number of observed birds, and wrack index (defined as the abundance of fresh surf-cast kelp on the beach, where a rating of 1 would be the least amount of wrack, and 5 is the highest amount of wrack)."), 
+                                      h1("How to Use"), 
+                                      p("Our data is assessed at two spatial scales. VAFB is split into three beach regions: North, Purisima, and South Beaches. These regions are further split into beach sections, split in the field by rivers or access trails. The map on the second tab shows the location of the beach sections and their three letter code. North Beaches labels are in blue, Purisima Beaches in yellow, and South Beaches in green"),
+                                      p("The first tab summarizes abundance of four bird families (Shorebirds, Gulls, Terns, and Raptors) over time at the three beach regions of VAFB. A date range can be selected to assess population at different time scales. The second tab assesses the population of Western snowy plovers, sanderlings, and all gulls relative to brown algal wrack abundance at the beach section scale. The third tab summarizes species diversity for each beach section at a selected time scale."),
                                       h1("Authors"), 
-                                      p("This app was created in Shiny by Jamie Miler and Kristan Culbert for ESM 244 (Advanced Data Analysis) - Winter 2019. Photo by Ross Griswold."))),
+                                      p("This app was created in Shiny by Jamie Miller and Kristan Culbert for ESM 244 (Advanced Data Analysis) - Winter 2019. Photo by Ross Griswold. Map is from Robinette et al. 2017"))),
                            
                            # Second Tab Panel = Time and Species Count Data
                            
@@ -67,7 +68,7 @@ ui <- fluidPage(theme = shinytheme("superhero"), #Valid themes: cerulean, cosmo,
                            
                            
                            tabPanel("Species Abundance Over Time",
-                                    h1("Species and Species Type by Beach - Time Series"),
+                                    h1("Species Abundance Over Time"),
                                     sidebarLayout(
                                       sidebarPanel(
                                        
@@ -114,7 +115,8 @@ ui <- fluidPage(theme = shinytheme("superhero"), #Valid themes: cerulean, cosmo,
                                                  )
                                         )
                                       )
-                                    )),
+                                    )), 
+                        
 
 # Fourth Tab Panel = Time and Wrack Data, SNPL/SAND/gull graphs
 
@@ -132,30 +134,21 @@ ui <- fluidPage(theme = shinytheme("superhero"), #Valid themes: cerulean, cosmo,
 
 #App users will be able to use the time slider to scroll through wrack index data and shorebird count data, which will generate a line graph. 
 
-tabPanel("Habitat Change Over Time", 
-         h1("Shorebird, Gull, and Relative Wrack Abundance - Time Series"),
+tabPanel("Habitat and Species Change Over Time", 
+         h1("Habitat and Species Change Over Time"),
          
          # Sidebar with a slider input for number of bins 
          sidebarLayout(
            sidebarPanel(
-                         img(src='VAFB_small.jpg', align = "left")
-             ),
-           
-           
-           # Output: use radio buttons for site location, radio buttons for bird species to create a line plot
-           mainPanel(
-             fluidRow(
-
-               column(3, offset = 1,
-                      
+             
              radioButtons("species_type_3", 
                           label = "Select Species:",
                           choices = list("Snowy Plover" = "SNPL", 
                                          "Sanderling" = "SAND",
-                                         "Gulls" = "Gull"))
-             ),
+                                         "Gulls" = "Gull")),
              
-             column(3,
+             
+             
              
              selectInput("beach_3", 
                          label = "Select Beach Section:", 
@@ -167,23 +160,28 @@ tabPanel("Habitat Change Over Time",
                                         "Wall Beach (WAL)" = "WAL",
                                         "Surf North (SNO)" = "SNO",
                                         "Surf South (SSO)" = "SSO"))
-             )
-           ),
+
+             ),
            
-             hr(),
            
-           plotOutput("wrack_plot"),
-           plotOutput("wrack_avian_plot")
+           # Output: use radio buttons for site location, radio buttons for bird species to create a line plot
+           mainPanel(
+             plotOutput("wrack_plot"),
+             plotOutput("wrack_avian_plot")
              
          ))),
 
 
-# Regression Analysis
+# Species Diversity
 
 tabPanel("Species Diversity",
-         h1("Seasonal Species Diversity by Beach"),
-         sidebarLayout(
-           sidebarPanel(
+         h1("Species Diversity"),
+         
+         mainPanel(
+           
+           fluidRow(
+               
+               column(4,
 
              dateRangeInput("survey_week_5", 
                             label = "Choose Date Range:",
@@ -191,9 +189,20 @@ tabPanel("Species Diversity",
                             end = "2019-03-01",
                             min = "2011-03-01", 
                             max ="2019-03-01")
-           ),
-           
+             ),
+
+                          column(4,
+                    
+                    selectInput("species_type_5", 
+                                label = "Select Avian Family:",
+                                choices = list("Shorebird" = "Shorebird", 
+                                               "Gull" = "Gull", 
+                                               "Tern" = "Tern",
+                                               "Raptor" = "Raptor"))
+             ),
              
+             column(4,
+
              selectInput("beach_5", 
                          label = "Select Beach Section:", 
                          choices = list("Minuteman (MIN)" = "MIN",
@@ -204,17 +213,13 @@ tabPanel("Species Diversity",
                                         "Wall Beach (WAL)" = "WAL",
                                         "Surf North (SNO)" = "SNO",
                                         "Surf South (SSO)" = "SSO"))
-           ),
-           
-           
+           )),
+          
            # Output: use year input, radio buttons for site location, radio buttons for bird species to create a line plot
-           mainPanel(
+
              plotOutput("avian_diversity_plot")
            )
-         ))
-
-)
-
+         )))
 
 
 
@@ -323,8 +328,6 @@ server <- function(input, output) {
         geom_point() +
         geom_smooth() +
  #       geom_line(aes(color = mean_wrack)) +
-#        scale_fill_manual(limits = c("Snowy Plover", "Sanderling", "Gulls"), 
- #                         values = c("deepskyblue4","darkolivegreen4","darkgoldenrod2")) +
         scale_y_continuous(expand = c(0,0)) +
         scale_x_date(breaks = as.Date(c("2012-03-01","2012-09-01",
                                         "2013-03-01","2013-09-01",
@@ -345,17 +348,16 @@ server <- function(input, output) {
         labs(x = " ", y = "Mean Wrack Score") +
         #      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
         theme_classic()
-    }, height = 300, width = 1000)
+    }, height = 350, width = 1000)
   
   
     output$wrack_avian_plot <- renderPlot({
       
       ggplot(wrack_avian(), 
              aes(x = survey_week, y = total)) +
-        geom_col() +
- #       geom_line(aes(color = mean_wrack)) +
-#        scale_fill_manual(limits = c("Snowy Plover", "Sanderling", "Gulls"), 
- #                         values = c("deepskyblue4","darkolivegreen4","darkgoldenrod2")) +
+        geom_col(aes(fill = species_2), show.legend = FALSE) +
+        scale_fill_manual(limits = c("SNPL", "SAND", "Gull"), 
+                          values = c("deepskyblue4","darkolivegreen4","darkgoldenrod2")) +
         scale_y_continuous(expand = c(0,0)) +
         scale_x_date(breaks = as.Date(c("2012-03-01","2012-09-01",
                                         "2013-03-01","2013-09-01",
@@ -374,9 +376,8 @@ server <- function(input, output) {
                                 "03/2018","09/2018",
                                 "03/2019")) +
         labs(x = "Date", y = "Number Birds Observed") +
-        #      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
         theme_classic()
-    }, height = 400, width = 1000)
+    }, height = 350, width = 1000)
 
  #######################################   
     # Species diversity
@@ -395,24 +396,44 @@ server <- function(input, output) {
                  | beach_section_initials == input$beach_5[5]
                  | beach_section_initials == input$beach_5[6]
                  | beach_section_initials == input$beach_5[7]
-                 | beach_section_initials == input$beach_5[8]) 
+                 | beach_section_initials == input$beach_5[8]) %>%
+          filter(species_type == input$species_type_5[1] | 
+                   species_type == input$species_type_5[2] | 
+                   species_type == input$species_type_5[3] |
+                   species_type == input$species_type_5[4])
       }
     )
-    
-    
     
     output$avian_diversity_plot <- renderPlot({
       
       ggplot(avian_diversity(), 
              aes(x = survey_week, y = total, fill = species)) +
-        geom_bar(stat = "identity", position = "fill")
-    })
-      
-  
+        geom_bar(stat = "identity", position = "fill") + 
+#        scale_color_brewer(palette = "Spectral") + # Not working
+        scale_y_continuous(expand = c(0,0)) +
+        scale_x_date(breaks = as.Date(c("2011-03-01","2011-09-01",
+                                        "2012-03-01","2012-09-01",
+                                        "2013-03-01","2013-09-01",
+                                        "2014-03-01","2014-09-01",
+                                        "2015-03-01","2015-09-01",
+                                        "2016-03-01","2016-09-01",
+                                        "2017-03-01","2017-09-01",
+                                        "2018-03-01","2018-09-01",
+                                        "2019-03-01")), 
+                     labels = c("03/2011","09/2011",
+                                "03/2012","09/2012",
+                                "03/2013","09/2013",
+                                "03/2014","09/2014",
+                                "03/2015","09/2015",
+                                "03/2016","09/2016",
+                                "03/2017","09/2017",
+                                "03/2018","09/2018",
+                                "03/2019")) +
+        labs(x = "Date", y = "Proportion of Species") +
+#        theme(legend.position="bottom") + # Not working
+        theme_classic()
+    }, height = 500, width = 1500)
 }
-
-
-
 
 # Run the application 
 shinyApp(ui = ui, server = server)
